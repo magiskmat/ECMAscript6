97d99d2abd7a8d408c48c875f973a354521e4944
johannahakansson <magiskmat@gmail.com>
2019-03-12 15:28:17 +0100

    uppdateringar

--- Ch02/02_04/start/webpack.config.js
+++ Ch02/02_04/start/webpack.config.js
@@ -1,9 +1,17 @@
+var webpack = require("webpack");
+
 module.exports = {
 	entry: './script.js',
 	output: {filename: 'bundle.js'},
 	module: {
-		loaders: [
-			{test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/}
+		rules: [
+			{
+				test: /\.js?/,
+				loader: 'babel-loader', 
+				exclude: /node_modules/}
+				query: {
+					presets: ["env"]
+				}
 		]
 	}
 };
